a1007ee483be19547719a3746adfa6560c530141
Bryson Elliott <brysonelliott@Brysons-MBP.lan>
2021-08-27 12:29:15 -0600

    Final attempt on Password

--- password.js
+++ password.js
@@ -1,14 +1,15 @@
 //Password Login
 
 class Password {
-    constructor(passwordLength) {
+    constructor(password, passwordLength) {
     this.password = password
     this.passwordLength = passwordLength
     }
 }
-for (let i = 0; i > 10; i++) {
-if (i > 10){
+for (let i = 0; i >= 10; i++) {
+if (passwordLength >= 10){
     console.log(`Password is correct`)
 } else console.log(`Password is incorrect`)
 }
-let passwordLength = `Brilliant!`
+let password = `Mitochondria`
+console.log(password)
